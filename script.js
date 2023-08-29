19cc06e953ddf05c447b54aafb4fdbb5d8e0c342
Giovanni Santana <gjsantana2002@gmail.com>
2023-08-29 05:54:25 -0400

    dropdown

--- script.js
+++ script.js
@@ -10,3 +10,16 @@ linkTitle.forEach((link) => {
       .classList.toggle("footer-link-active");
   });
 });
+
+const navBtn = document.querySelectorAll(".nav-link--btn");
+
+navBtn.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    if (
+      e.target.classList.contains("nav-link--btn") ||
+      e.target.classList.contains("md")
+    ) {
+      btn.querySelector(".nav-dropdown").classList.toggle("nav-drop-close");
+    }
+  });
+});
